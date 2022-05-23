4844fbbb6aa408229719e79df373ac3ce0ee584d
Leo Prado <leonardodoprado231@gmail.com>
2022-05-23 10:36:14 -0300

    Estruturando os arquivos de view e estilização

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const routes = require('./routes');
 const db = require('@database/db')
 const app = express();
+const path = require('path');
+
+// View engine
+app.set('view', path.join(__dirname, 'view'))
+app.set('view engine', 'ejs');
 
 app.use('/', routes);
 
