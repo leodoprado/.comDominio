cd6720960d35f69028eb2698476edc9c059b2474
Leo Prado <leonardodoprado231@gmail.com>
2022-06-01 15:52:04 -0300

    Puxando informações salvas no banco

--- src/controller/adm/admPagePerfil.js
+++ src/controller/adm/admPagePerfil.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const router = express.Router()
+const dadosConta = require("@model/loginModel");
 const accessValidation = require("@middleware/accessValidation");
 
 router.get('/login/administrador/perfil', accessValidation ,(req, res) => {
-    res.render("log/adm/perfilAdministrador")
-})
+    dadosConta.findAll({
+        attributes: ['idUser', "nivelAcesso"]
+    }).then(function(dados){
+        res.render("log/adm/perfilAdministrador", {dados: dados})
+    });
+});
 
 module.exports = router;
