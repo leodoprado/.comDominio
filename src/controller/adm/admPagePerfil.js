fbcac0b317aaae732eccc7a79e903fec9eac7073
Leo Prado <leonardodoprado231@gmail.com>
2022-06-03 09:47:14 -0300

    Alterações back-end

--- src/controller/adm/admPagePerfil.js
+++ src/controller/adm/admPagePerfil.js
@@ -13,4 +13,22 @@ router.get('/login/administrador/perfil', accessValidation ,(req, res) => {
     });
 });
 
+router.post('/login/administrador/perfil/save', accessValidation, (req, res) => {
+    // definindo as variaveis que vão ser pegas do formulário
+    var nome = req.body.nome;
+    var datanascimento = req.body.datanascimento;
+    var cpf = req.body.cpf;
+    var rg = req.body.rg;
+    var estadocivil = req.body.estadocivil;
+    var sexo = req.body.sexo;
+    var pais = req.body.pais;
+    var cidadenatal = req.body.cidadenatal;
+    var estado = req.body.estado;
+
+    var email = req.body.email;
+    var emailcomplementar = req.body.email;
+    var telefone1 = req.body.telefone1;
+    var telefone2 = req.body.telefone2;
+})
+
 module.exports = router;
