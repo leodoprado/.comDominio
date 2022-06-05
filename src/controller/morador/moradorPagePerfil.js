f3607ad6b580c11eb6269a0b93f6cfa260ed9596
Leo Prado <leonardodoprado231@gmail.com>
2022-06-05 11:20:54 -0300

    Atualizando login do morador

--- src/controller/morador/moradorPagePerfil.js
+++ src/controller/morador/moradorPagePerfil.js
@@ -1,9 +1,66 @@
 const express = require("express")
 const router = express.Router()
 const accessValidation = require("@middleware/accessValidation");
+const PerfilUser = require("@model/perfilModel");
 
-router.get('/login/morador/perfil', accessValidation ,(req, res) => {
-    res.render("log/morador/perfil")
+router.get('/login/morador/perfil/:id', accessValidation ,(req, res) => {
+    const session = req.session.user
+
+    id = req.params.id;
+    res.cookie('id', id, {expire: new Date()+10*60*1000})
+
+    PerfilUser.findOne({where: {id: id, idUser: session.idUser,  nivelAcesso: session.nivelAcesso}}).then(function(result){
+        if(!result) {
+            res.redirect(`/login`)
+        } else {
+            res.render('log/morador/perfilMorador', { result: result})
+        }
+    })
+});
+
+router.post('/login/morador/perfil/:id', (req, res) => {
+
+    nome = req.body.nome
+    datanascimento = req.body.datanascimento
+    cpf = req.body.cpf;
+    rg = req.body.rg;
+    estadocivil = req.body.estadocivil;
+    sexo = req.body.sexo;
+    pais = req.body.pais;
+    cidadenatal = req.body.cidadenatal;
+    estado = req.body.estado;
+    email = req.body.email;
+    emailcomplementar = req.body.emailcomplementar;
+    telefone1 = req.body.telefone1;
+    telefone2 = req.body.telefone2;
+
+    console.log(nome)
+
+    if(req.body !== ''){
+        PerfilUser.update({
+            nome: nome,
+            datanascimento: datanascimento,
+            cpf: cpf,
+            rg : rg,
+            estadocivil : estadocivil,
+            sexo : sexo,
+            pais : pais,
+            cidadenatal : cidadenatal,
+            estado: estado,
+            email : email,
+            emailcomplementar : emailcomplementar,
+            telefone1 : telefone1,
+            telefone2 : telefone2
+        },
+        {
+            where: {id : id}
+        });
+        console.log(nome)
+        res.clearCookie('id');
+        res.redirect('/login/morador/perfil/:id');
+    } else {
+        res.render('log/morador/perfilMorador', { result : req.body})
+    }
 })
 
 module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -12,6 +12,8 @@ router.post('/authenticate', loginPageController);
 router.get('/logout', loginPageController);
 router.get('/create', create);
 router.post('/create/user', create);
+router.get('/login/morador/perfil/:id', moradorPagePerfilController);
+router.post('/login/morador/perfil/:id', moradorPagePerfilController)
 router.get('/login/administrador/perfil/:id', admPagePerfilController);
 router.post('/login/administrador/perfil/:id', admPagePerfilController)
 
