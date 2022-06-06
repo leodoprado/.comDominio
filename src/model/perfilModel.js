a4a7389e44663b64ce86fc52f80c15c3c2361cb0
Leo Prado <leonardodoprado231@gmail.com>
2022-06-06 15:01:39 -0300

    Atualizando o back-end do projeto

--- src/model/perfilModel.js
+++ src/model/perfilModel.js
@@ -15,56 +15,68 @@ const PerfilUser = connection.define('perfil-user', {
     },
     nome: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false,
+        defaultValue: "-"
     },
     datanascimento: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false,
+        defaultValue: "00/00/0000"
     },
     cpf: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false,
+        defaultValue: "000-000-000.00"
     },
     rg: {
         type: Sequelize.INTEGER,
-        allowNull: true
+        allowNull: false,
+        defaultValue: "0000000000"
     },
     estadocivil: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false,
+        defaultValue: "-"
     },
     sexo: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false,
+        defaultValue: "-"
     },
     pais: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false,
+        defaultValue: "-"
     },
     cidadenatal: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false,
+        defaultValue: "-"
     },
     estado: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false,
+        defaultValue: "--"
     },
     email: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false,
+        defaultValue: "-"
     },
     emailcomplementar: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false,
+        defaultValue: "-"
     },
     telefone1: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false,
+        defaultValue: "(00)00000-0000"
     }, 
     telefone2: {
         type: Sequelize.STRING,
-        allowNull: true,
-        
+        allowNull: false,
+        defaultValue: "(00)00000-0000"
     }
 })
 
